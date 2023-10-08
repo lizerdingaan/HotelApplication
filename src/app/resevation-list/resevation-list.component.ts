8f7fae8f604692c229a99a66e331b6a90f39e4cc
lizerdingaan <tshegofatsodingaan45@gmail.com>
2023-10-08 18:38:57 +0200

    Added resevation list to the list component

--- src/app/resevation-list/resevation-list.component.ts
+++ src/app/resevation-list/resevation-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ResevationService } from "../resevation/resevation.service";
+import { Resevation } from "../models/resevation";
 
 @Component({
   selector: 'app-resevation-list',
   templateUrl: './resevation-list.component.html',
   styleUrls: ['./resevation-list.component.css']
 })
-export class ResevationListComponent {
+export class ResevationListComponent implements OnInit{
+
+  resevations: Resevation[] = [];
+
+  constructor(private resevationService: ResevationService) {
+  }
+  ngOnInit(): void {
+    this.resevations = this.resevationService.getResevations();
+  }
 
 }
