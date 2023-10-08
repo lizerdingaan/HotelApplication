c55054598d37a9d3512a5cfce959f8bf343dc62f
lizerdingaan <tshegofatsodingaan45@gmail.com>
2023-10-08 17:36:49 +0200

    adding CRUD operations in the service

--- src/app/resevation/resevation.service.ts
+++ src/app/resevation/resevation.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import {Resevation} from "../models/resevation";
+import {findIndex} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResevationService {
 
-  constructor() { }
+  private resevations: Resevation[] = [];
+
+  // CRUD
+
+  getResevations(): Resevation[] {
+    return this.resevations;
+  }
+
+  getResevation(id: string): Resevation | undefined{
+    return this.resevations.find(res => res.id === id);
+  }
+
+  addResevation(resevation: Resevation) {
+    this.resevations.push(resevation)
+  }
+
+  deleteResevation(id: string) {
+    let index = this.resevations.findIndex(res => res.id === id);
+    this.resevations.splice(index, 1);
+  }
+
+  updateResevation(updatedResevation: Resevation) {
+    let index = this.resevations.findIndex(res => res.id === updatedResevation.id);
+    this.resevations[index] = updatedResevation;
+  }
 }
